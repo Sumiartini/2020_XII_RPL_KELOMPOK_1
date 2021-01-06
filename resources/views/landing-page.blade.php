69097eb6ca540773329206b53eb07e5fadb9c1a8
sumiartini <sumiartinisrirahayu@gmail.com>
2021-01-06 16:33:27 +0700

    [W]Memperbaiki error js di landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -10,12 +10,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-
-    <script src="assets/js/jquery.js"></script> 
-    <script src="assets/js/popper.js"></script> 
-    <script src="assets/js/bootstrap.js"></script>
-
+   
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
--- resources/views/teachers/registration-teacher.blade.php
+++ resources/views/teachers/registration-teacher.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Rocker - Bootstrap4 Admin Dashboard Template</title>
+    <title>Formulir Guru</title>
     <!--favicon-->
     <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
     <!-- simplebar CSS-->
