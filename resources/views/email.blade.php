88626fab58e2862bc25b5950ecfda195396d2508
sumiartini <sumiartinisrirahayu@gmail.com>
2021-01-10 19:07:28 +0700

    [w]memperbaiki halaman pesan verifikasi di  email

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,44 +1,17 @@
-<!-- <center>
-    <h1><b>Halo {{ $user->usr_name }}</b></h1> <br>
-    <strong> SELAMAT DATANG DI WEB SMK MAHAPUTRA CERDAS UTAMA </strong> <br>
-    Perhatikan Syarat-Syarat Berikut Ini !!!
-    <img src=https://drive.google.com/file/d/1PTq_YDAJhQVm3UT6EgPv5NZWUAXzBOHn/view?usp=sharingy>
-    Silahkan klik link di bawah ini untuk memverifikasi email anda <br>
-    <a href="{{url('/account/'.$user->usr_id.'/'.$user->usr_verification_token.'/activate')}}"> Klik Disini </a>
-</center> -->
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta>
+<html lang="en">
+  <head>
     <title></title>
-    <meta>
-</head>
-<body>
-    <table width="100%">
-    <tr>
-        <td>
-            <center>
-              <h1 style=""> Halo {{ $user->usr_name }} </h1><br>
-              <h3 style="color:blue;" >SELAMAT DATANG DI WEB SMK MAHAPUTRA CERDAS UTAMA</h3>
-            </center>
-        </td>
-    </tr>
-    <tr>
-        <td style="padding: 20px 0 30px 0;">
-        <center>Perhatikan Syarat-syarat berikut ini !!!
-          <p>Mengisi formulir pendaftaran dengan data yang benar <br>
-            Siapkan foto formal calon siswa <br>
-          No telepon dan email harus dapat dihubungi</p>
-         
-        Silahkan klik link di bawah ini untuk memverifikasi email anda <br>
-      <a class="btn btn-primary" href="{{url('/account/'.$user->usr_id.'/'.$user->usr_verification_token.'/activate')}}"> Klik Disini </a>
-             </center>
-        </td>
-    </tr>
-</table>
-</body>
+  </head>
+  <body>
+    <p style="text-align: center;"><strong><img src="https://1.bp.blogspot.com/-XcI6X4NxTQ4/X_rgwpqnmnI/AAAAAAAAQ4k/V_OgTlNvfcg32GUbBGSfz4793uv_hTyaQCLcBGAsYHQ/w945-h600-p-k-no-nu/logo.png" alt="" width="170" height="170" /></strong></p>
+    <p style="text-align: center; color: black; font-size:20px; font-family: 'Times New Roman'"><strong>Halo {{ $user->usr_name }}</strong>&nbsp;</p>
+    <p style="text-align: center; color: black; font-size:20px; font-family: 'Times New Roman'"><strong>SELAMAT DATANG DI WEB SMK MAHAPUTRA CERDAS UTAMA</strong></p>
+    <p style="text-align: center; color: black; font-size:20px; font-family: 'Times New Roman'">&nbsp;Silahkan klik link di bawah ini untuk memverifikasi email anda</p>
+    <p style="text-align: center; font-size:20px;"><a href="{{url('/account/'.$user->usr_id.'/'.$user->usr_verification_token.'/activate')}}">
+      <button> Klik Disini </button></a>
+    </p>
+    <p style="text-align: center; color: black; font-size:20px; font-family: 'Times New Roman'">&nbsp;<strong>Perhatikan Syarat-Syarat Berikut Ini !!!</strong></p>
+    <p><img style="display: block; margin-left: auto; margin-right: auto;" src="https://1.bp.blogspot.com/-XOMQEFS3wnY/X_rOJAN8GPI/AAAAAAAAQ4Y/9o4OJricokMgOtubbNCBUiNUKAmdBubbACLcBGAsYHQ/w468-h330/z.jpeg" alt="" width="462" height="340" /></p>
+  </body>
+</html>
 
-</html>  	
-    	
-    
--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -457,7 +457,7 @@ footer{
                                 </div>
                             </div>
                             <div class="carousel-item">
-                                <img src="assets/images/3.jpg" class="d-block w-100" alt="...">
+                                <img src="assets/images/z.jpeg" class="d-block w-100" alt="...">
                                 <div class="carousel-caption d-none d-md-block">
                                 </div>
                             </div>
