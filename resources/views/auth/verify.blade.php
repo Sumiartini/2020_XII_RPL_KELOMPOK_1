90e1a2933152c80206e127e59163729d4fcbcc67
sumiartini <sumiartinisrirahayu@gmail.com>
2021-01-10 16:24:37 +0700

    [w]memperbaiki halaman tunggu verifikasi email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header" style="background-color: skyblue;">{{ __('Silahkan Verifikasi Email Anda') }}</div>
                 <div class="card-body">
                     @if ($message = Session::get('success'))
-                    <div class="alert alert-success alert-block">
+                    <div class="alert alert-primary alert-block">
                         <button type="button" class="close" data-dismiss="alert">×</button>
                         <strong>{{ $message }}</strong>
                     </div>
@@ -16,14 +16,11 @@
                     <form id="resendVerification" action="/account/resend-verification" method="POST">
                         @csrf
                         akun anda belum diverifikasi, harap cek email anda termasuk spam pada email anda <br>
-                        <input type="submit" name="resend-verification" value="Kirim ulang email verifikasi" id="btnSubmit">
+                        <input type="submit" class="btn btn-info btn-round btn-sm waves-effect waves-light m-1" name="resend-verification" style="background-color: skyblue; float: right;" value="Kirim ulang email verifikasi" id="btnSubmit">
                     </form>
                 </div>
             </div>
         </div>
     </div>
-
-
-
 
     @endsection
--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,16 +1,44 @@
-<center>
+<!-- <center>
     <h1><b>Halo {{ $user->usr_name }}</b></h1> <br>
     <strong> SELAMAT DATANG DI WEB SMK MAHAPUTRA CERDAS UTAMA </strong> <br>
     Perhatikan Syarat-Syarat Berikut Ini !!!
-   <strong>
-   	<p>Mengisi Formulir Pendaftaran dengan Data yang Benar</p>
-	<p>Siapkan Foto Formal Calon Siswa</p>
-	<p>No telepon dan Email harus dapat Dihubungi</p>
-	</strong>
+    <img src=https://drive.google.com/file/d/1PTq_YDAJhQVm3UT6EgPv5NZWUAXzBOHn/view?usp=sharingy>
     Silahkan klik link di bawah ini untuk memverifikasi email anda <br>
     <a href="{{url('/account/'.$user->usr_id.'/'.$user->usr_verification_token.'/activate')}}"> Klik Disini </a>
-</center>
-   
-  	
+</center> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta>
+    <title></title>
+    <meta>
+</head>
+<body>
+    <table width="100%">
+    <tr>
+        <td>
+            <center>
+              <h1 style=""> Halo {{ $user->usr_name }} </h1><br>
+              <h3 style="color:blue;" >SELAMAT DATANG DI WEB SMK MAHAPUTRA CERDAS UTAMA</h3>
+            </center>
+        </td>
+    </tr>
+    <tr>
+        <td style="padding: 20px 0 30px 0;">
+        <center>Perhatikan Syarat-syarat berikut ini !!!
+          <p>Mengisi formulir pendaftaran dengan data yang benar <br>
+            Siapkan foto formal calon siswa <br>
+          No telepon dan email harus dapat dihubungi</p>
+         
+        Silahkan klik link di bawah ini untuk memverifikasi email anda <br>
+      <a class="btn btn-primary" href="{{url('/account/'.$user->usr_id.'/'.$user->usr_verification_token.'/activate')}}"> Klik Disini </a>
+             </center>
+        </td>
+    </tr>
+</table>
+</body>
+
+</html>  	
     	
     
